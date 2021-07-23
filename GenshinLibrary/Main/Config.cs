a23a967328042a907ebf94bc0f64d055532dedd8
Carrak <51882360+Carrak@users.noreply.github.com>
2021-07-23 17:17:48 +0400

    Store Config in an object of a special class

---
+++ GenshinLibrary/Main/Config.cs
@@ -0,0 +1,15 @@
+﻿using GenshinLibrary.Calculators.PrimogemCalculator;
+using Newtonsoft.Json;
+
+namespace GenshinLibrary
+{
+    public class Config
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+        [JsonProperty("connection")]
+        public string Connection { get; set; }
+        [JsonProperty("current_version")]
+        public Version Version { get; set; }
+    }
+}
