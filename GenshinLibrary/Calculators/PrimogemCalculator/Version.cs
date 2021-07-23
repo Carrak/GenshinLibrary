0ba0e14cbfcf1add8e2696052acde2232b223f38
Carrak <51882360+Carrak@users.noreply.github.com>
2021-07-23 17:18:26 +0400

    Parse Major and Minor from Version Name and make it possible to parse from JSON

--- GenshinLibrary/Calculators/PrimogemCalculator/Version.cs
+++ GenshinLibrary/Calculators/PrimogemCalculator/Version.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace GenshinLibrary.Calculators.PrimogemCalculator
 {
@@ -7,12 +8,22 @@ namespace GenshinLibrary.Calculators.PrimogemCalculator
         public string VersionName { get; }
         public DateTime Start { get; }
         public DateTime End { get; }
+        public int Major { get; }
+        public int Minor { get; }
 
-        public Version(DateTime start, DateTime end, string versionName)
+        [JsonConstructor]
+        public Version(
+            [JsonProperty("start")] DateTime start, 
+            [JsonProperty("end")] DateTime end,
+            [JsonProperty("version_name")] string versionName)
         {
             Start = start;
             End = end;
             VersionName = versionName;
+
+            var split = versionName.Split('.');
+            Major = int.Parse(split[0]);
+            Minor = int.Parse(split[1]);
         }
 
         public string Info()
