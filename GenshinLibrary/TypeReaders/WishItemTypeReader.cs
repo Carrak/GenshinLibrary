129222efa7e8dfce229441eaa6db29915738a80b
Carrak <51882360+Carrak@users.noreply.github.com>
2021-07-30 17:03:39 +0400

    Use suggestion instead of showing it in error

--- GenshinLibrary/TypeReaders/WishItemTypeReader.cs
+++ GenshinLibrary/TypeReaders/WishItemTypeReader.cs
@@ -1,4 +1,5 @@
 ﻿using Discord.Commands;
+using GenshinLibrary.Models;
 using GenshinLibrary.Services.Wishes;
 using System;
 using System.Threading.Tasks;
@@ -10,10 +11,12 @@ namespace GenshinLibrary.TypeReaders
         public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
         {
             var _wishes = (WishService)services.GetService(typeof(WishService));
-            if (_wishes.WishItems.TryGetValue(input, out var wishItem))
-                return Task.FromResult(TypeReaderResult.FromSuccess(wishItem));
+            if (_wishes.WishItems.TryGetValue(input, out var wishItem1))
+                return Task.FromResult(TypeReaderResult.FromSuccess(wishItem1));
+            else if (_wishes.GetBestSuggestion(input) is WishItem wishItem2)
+                return Task.FromResult(TypeReaderResult.FromSuccess(wishItem2));
             else
-                return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, $"No such item has been found. Did you mean `\"{_wishes.GetBestSuggestion(input).Name}\"`?"));
+                return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, $"No such wish item has been found."));
         }
     }
 }
