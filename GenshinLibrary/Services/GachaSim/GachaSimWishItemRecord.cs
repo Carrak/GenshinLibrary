d9dcba11f58816b58971983944ef4b0b37ad3444
Carrak <51882360+Carrak@users.noreply.github.com>
2021-12-05 19:15:48 +0400

    Don't inherit from WishItemRecord

--- GenshinLibrary/Services/GachaSim/GachaSimWishItemRecord.cs
+++ GenshinLibrary/Services/GachaSim/GachaSimWishItemRecord.cs
@@ -3,15 +3,17 @@ using System;
 
 namespace GenshinLibrary.Services.GachaSim
 {
-    public class GachaSimWishItemRecord : WishItemRecord
+    public class GachaSimWishItemRecord
     {
+        public WishItem WishItem { get; }
         public int FiveStarPity { get; }
         public int FourStarPity { get; }
 
-        public GachaSimWishItemRecord(int fivestarPity, int fourstarPity, DateTime dateTime, WishItem wishItem) : base(dateTime, wishItem)
+        public GachaSimWishItemRecord(int fivestarPity, int fourstarPity, WishItem wishItem)
         {
             FiveStarPity = fivestarPity;
             FourStarPity = fourstarPity;
+            WishItem = wishItem;
         }
     }
 }
